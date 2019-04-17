55917524616d91f38af8288cea2bb9a33477669d
Mini Mel <25712940+Mini-mel@users.noreply.github.com>
2019-04-17 16:03:19 -0700

    groundwork for timer functionality

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ const {token} = require('./config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-
 bot.on('ready', () => {
-    console.log("ready!");
+    console.log("Ready!");
 });
-
-function Confirmmsg(){
-    console.log("done!");
-}
 
 bot.on('message', (msg) => {
 
     //checks for hello command and responds with "world!"
     if (msg.content == "hello"){
         msg.reply("world!");
-        msg.edit('This is my new content!')
-            .then(msg => console.log(`New message content: ${msg}`))
-            .catch(console.error);
     }
-
+    
+    //starts timer, then when timer ends, edits its message
     if (msg.content == "starttime"){
-        setTimeout(Confirmmsg, 3000);
+        msg.channel.send("Start!").then((msg)=>{
+            setTimeout(function(){msg.edit("Completed!");}, 3000)
+       });
     }
 });
 
