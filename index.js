3649a1eceaca90de2cef64cf4306976960f893e5
Mini Mel <25712940+Mini-mel@users.noreply.github.com>
2019-04-16 22:41:11 -0700

    wait 5 seconds then console logs done (groundwork for future timer implementation)

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const {token} = require('./config.json');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+
 bot.on('ready', () => {
     console.log("ready!");
 });
 
+function Confirmmsg(){
+    console.log("done!");
+}
+
 bot.on('message', (msg) => {
+
     //checks for hello command and responds with "world!"
-    if (msg.content == "!hello"){
+    if (msg.content == "hello"){
         msg.reply("world!");
+        msg.edit('This is my new content!')
+            .then(msg => console.log(`New message content: ${msg}`))
+            .catch(console.error);
+    }
+
+    if (msg.content == "starttime"){
+        setTimeout(Confirmmsg, 3000);
     }
 });
 
